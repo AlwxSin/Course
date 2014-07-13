6e4f903889ea7bd652438b17adada98c65051c03
Alwx <alwxsin@gmail.com>
2014-07-13 14:14:19 +0400

    Changed wrong maxPrime function Created new eff_fibo function

--- 2ndWeek/2_maximum_prime.py
+++ 2ndWeek/2_maximum_prime.py
@@ -2,20 +2,27 @@
 # Которая принимает на вход какое-то натуральное число,
 # А возвращает в качестве результата его наибольший простой множитель.
 
+# Нужна функция для определения простое ли число
+
+def isprime(m):
+    if m == 2:                                  # Исключаем 2
+        return True
+    if m < 2 or m % 2 == 0:                     # Исключаем 1 и все, что делится на 2
+        return False
+    for i in range(3, int(m ** 0.5) + 1, 2):    # Нужно перебрать числа, не превосходящие корня от искомого
+        if m % i == 0:
+            return False
+    return True
+
+# Теперь фунция нахождения наибольшего простого множителя
+
 def maximum_prime(n):
-    n = int(n)
+    if isprime(n) == True:
+        return n
     if n == 1:
-        return print("1 is not a prime")
-    if n == 2:
-        return n                                # Т.к. для двойки наибольший простой множитель будет двойка
-    if n >= 3:
-        min_prime = 3                           # Минимальное простое число для вычисления
-        prime_numbers = []                      # Список в который будут писаться все множители n
-        while n >= min_prime:
-            if n % min_prime == 0:
-                prime_numbers.append(min_prime) # Записываем по одному все найденые простые множители
-                n = n / min_prime
-            else:
-                min_prime += 2                  # Переход к следующему возможному простому множителю
-        return max(prime_numbers)
-                
+        return print("1 is not a Prime")
+    if n >= 2:
+        for i in range(2, int(n)):        # Перебираем возможные множители
+            if isprime(i) == True and n % i == 0:
+                x = i                     # Записываем множитель в переменную  
+        return x
--- 2ndWeek/4_eff_fibo.py
+++ 2ndWeek/4_eff_fibo.py
@@ -2,5 +2,5 @@ def fibo(n):
     numbers = [0, 1]                                # в этот список запишем все вычисления
     while len(numbers) != n:
         numbers.append(numbers[-1] + numbers[-2])   # Добавляем в конец списка сумму двух последних элементов
-    return max(numbers)
+    return(numbers[-1])
     
