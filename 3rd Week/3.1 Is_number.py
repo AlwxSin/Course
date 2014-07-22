81ffca88452f1ebd0b3cb4ab1544e30ce6c3e850
Alwx <alwxsin@gmail.com>
2014-07-22 13:57:35 +0400

    Changed 3.1 Cause it wasn't work

--- 3rd Week/3.1 Is_number.py
+++ 3rd Week/3.1 Is_number.py
@@ -1,9 +1,22 @@
 def is_number(x):
     """Функция будет проверять является ли строка x числом"""
-    if x.isdigit() == True:
-        return True
-    else:
-        return False
+    try:                # Пробуем привести данную строку x к числу с точкой
+        x = float(x)    
+        return True     # Если получилось
+    except ValueError:
+        return False    # Если выдает ошибку значения, которое нельзя представить в виде числа с точкой
 
 print(is_number('468947'))
 print(is_number("I'm an integer"))
+
+while True:
+    something = input('Enter something\n')
+    print(is_number(something))
+    if something == 'stop':
+        break
+
+
+    #if x.isdigit() == True:    # Не подходит, потому что
+    #    return True            # isdigit проверяет только цифры,
+    #else:                      # а не числа. на отрицательных или
+    #    return False           # не целых числах выдает False
