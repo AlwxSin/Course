5a132232fae2ad18dc5193b6151d40c37f7bd664
Alwx <alwxsin@gmail.com>
2014-07-22 17:48:01 +0400

    New generator function. Added names generator. Works quickly.

--- 3rd Week/3.3 Generator.py
+++ 3rd Week/3.3 Generator.py
@@ -1,15 +1,19 @@
-from random import randint
-import time
+from random import randint, choice
+from string import ascii_uppercase, ascii_lowercase
+#import time
 
 def generator(number_count):
-    number_list = []
-    start = time.clock()
-    while len(number_list) != number_count:
-        y = randint(1000000, 10000000)
-        if y in number_list: continue
-        else:
-            number_list.append(y)
-    finish = time.clock()
-    print(finish-start)
-    return True #number_list
+    """Generates a dict Name: Number. Number of strings = number_count"""
+    #start = time.clock()
+    phone_numbers = set()
+    name_list = set()
+    phone_book = {}
+    while len(phone_numbers) < number_count:
+        phone_numbers.add(randint(100000, 10000000)) # Random unique 7-digit numbers
+        name_list.add(str(choice(ascii_uppercase)) + str(''.join((choice(ascii_lowercase) for i in range(randint(4, 10)))))) # Random unique name, 4<len(name)<10
+    for i in range(number_count):
+        phone_book[name_list.pop()] = phone_numbers.pop() # Making a dict Name: Number
+    #finish = time.clock()
+    #print(finish-start)
+    return phone_book
         
