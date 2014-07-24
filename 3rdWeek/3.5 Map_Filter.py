56806737f32927e979942a347dd5b4a354ce590f
Alwx <alwxsin@gmail.com>
2014-07-24 11:33:16 +0400

    complete map and filter functions

--- 3rdWeek/3.5 Map_Filter.py
+++ 3rdWeek/3.5 Map_Filter.py
@@ -1,3 +1,17 @@
-# Don't forgent abou *args
-def my_map(Func, p1, *args):
-    
+L = [x for x in range(-2, 16)] # Test list
+
+def my_map(Func, arg):
+    result = []
+    for i in arg:
+        result.append(Func(i))
+    return result
+
+def my_filter(Func, arg):
+    result = []
+    for i in arg:
+        if Func(i) == True:
+            result.append(i)
+    return result
+
+print(my_map(lambda x: x*2, L))
+print(my_filter(lambda x: 2<x<15, L))
