317261ed09742c324981ac0e1595158455722986
Alwx <alwxsin@gmail.com>
2014-07-29 13:38:17 +0400

    Theory exercises

---
+++ Theory/1stLesson/1.1 OctToDec.py
@@ -0,0 +1,42 @@
+"""
+В восьмеричной системе счисления используется восемь цифр от 0 до 7.
+Напишите функцию, принимающую на вход строку в восьмеричной системе счисления,
+и выдающую в качестве результата число (в обычном Питоновском смысле).
+"""
+
+def OctToDec(x):
+    """Returns a decimal representation of a oct number"""
+    grade = len(x)-1
+    result = 0
+    for i in x:
+        if int(i) > 7:
+            return print('String must be in Octo')
+        result += int(i)*(8**grade)
+        grade -= 1
+    return result
+
+print(OctToDec('5'))
+print(OctToDec('0'))
+print(OctToDec('45'))
+print(OctToDec('453'))
+print(OctToDec('4542'))
+print(OctToDec('78315'))
+
+
+
+"""
+Ошибочный код. Сначала думал, что надо из Dec перевести в Oct.
+result = ""
+    if is_number(x) == True:
+        if 0 <= int(x) <= 7:
+            return x
+        elif int(x) < 0:
+            return print("Please, input a number above zero. I can't count negative numbers =(")
+        else:
+            while x:
+                result += str(int(x)%8)
+                x = int(x)//8
+            return result[::-1]
+    else:
+        return print('x is not a number')
+"""
---
+++ Theory/1stLesson/1.2 HexToSeven.py
@@ -0,0 +1,38 @@
+def chr_to_num(x):
+    x = ord(x)
+    if ord("0") <= x <= ord("9"): return x - ord("0")
+    elif ord("a") <= x <= ord("z"): return x - ord("a") + 10
+    else: return x - ord("A") + 10
+
+def to_seven(x):
+    """Returns a seven representation of a natural number"""
+    if x == None:
+        return False
+    result = ""
+    x = int(x)
+    while x:
+        result += str(x%7)
+        x //= 7
+    return result[::-1]
+
+def hex_to_dec(x):
+    result = 0
+    grade = len(x)-1
+    for i in x:
+        if chr_to_num(i) > 15:
+            return print('Not a hex')
+        else:
+            result += chr_to_num(i)*(16**grade)
+            grade -= 1
+    return str(result)
+
+def hex_to_seven(x):
+    x = hex_to_dec(x)
+    x = to_seven(x)
+    return x
+
+print(hex_to_seven('5'))
+print(hex_to_seven('68645'))
+print(hex_to_seven('68abcde45'))
+print(hex_to_seven('686AbCa5'))
+print(hex_to_seven('686hi45'))
