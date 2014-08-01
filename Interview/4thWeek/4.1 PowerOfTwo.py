1d7c9d180651fe644e7d25f9b648e794b8cd5645
Alwx <alwxsin@gmail.com>
2014-08-01 14:32:11 +0400

    improved for one line answer

--- Interview/4thWeek/4.1 PowerOfTwo.py
+++ Interview/4thWeek/4.1 PowerOfTwo.py
@@ -1,13 +1,13 @@
 def power_of_two(x):
     """Является ли число степенью двойки"""
-    if x == 1: return True
-    two = 2 << len(bin(x))-4
-    return x == two
+    two = x&(x-1)
+    return not two
     
 
 
-print(power_of_two(1))
-print(power_of_two(7))
-print(power_of_two(32))
-print(power_of_two(128))
-print(power_of_two(736))
+print(power_of_two(1))  #True
+print(power_of_two(2))  #True
+print(power_of_two(7))  #False
+print(power_of_two(32)) #True
+print(power_of_two(128))#True
+print(power_of_two(736))#False
