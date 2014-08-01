7adf95628a41ff2c3513abaae6b83a7e18f6f47f
Alwx <alwxsin@gmail.com>
2014-08-01 18:31:59 +0400

    Improved. No more additional lists

--- Interview/4thWeek/4.2 Reverse.py
+++ Interview/4thWeek/4.2 Reverse.py
@@ -8,13 +8,21 @@ def reverse(x):
     return result
 '''
 
-#Second try
-def reverse(x):
-    return reversed(x)
-
-
-
 x1 = [0,1,2,3,4,5,6,7,8,9]
 x2 = ['a','b','c','d','e','f','g','h','i','g']
+
+#Second try
+def reverse(x):
+    lenght = len(x)
+    for i in range(lenght-1, -1, -1):
+        x.append(x[i])
+    return x[lenght:]
+    
+
+
+
+
 print(reverse(x1))
 print(reverse(x2))
+
+
