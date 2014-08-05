faa3981952f0482137f184123121d23654bd789c
Alwx <alwxsin@gmail.com>
2014-08-05 10:54:57 +0400

    minor changes, added .gitignore

--- Python/5thWeek/rpg3.py
+++ Python/5thWeek/rpg3.py
@@ -7,8 +7,7 @@ class Character:
     self.health_max = 1
   def do_damage(self, enemy):
     damage = min(
-        max(randint(0, self.health) - randint(0, enemy.health), 0),
-        enemy.health)
+        max(randint(0, self.health) - randint(0, enemy.health), 0), enemy.health)
     enemy.health = enemy.health - damage
     if damage == 0: print("%s evades %s's attack." % (enemy.name, self.name))
     else: print("%s hurts %s!" % (self.name, enemy.name))
