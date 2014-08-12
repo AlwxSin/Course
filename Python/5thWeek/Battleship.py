53fe093647df59ac69edc8b7d178fec1af736b04
Alwx <alwxsin@gmail.com>
2014-08-12 18:12:27 +0400

    Some improvements, New functions

--- Python/5thWeek/Battleship.py
+++ Python/5thWeek/Battleship.py
@@ -1,18 +1,15 @@
 '''Battleship game'''
 from random import randint
-
-ch_coord = {}
-for i in range(0,10):
-    ch_coord[chr(i+ord('A'))] = i + 1
-    ch_coord[chr(i+ord('a'))] = i + 1
         
 class Battlefield:
-    def empty_field():
+    def __init__self(self):
+        self.name = 'Battlefield'
+    def empty_field(self):
         field=[]                                            # Empty array will fills for battlefield
-        for x in range(0,10):                               # First just zeros
-            field.append(['0' for i in range(10)])
-        field.insert(0,[i for i in range(11)])              # Number coordinates
-        for char in range(ord('A'),ord('K')):               # Character coordinates
+        for x in range(0,5):                                # First just zeros
+            field.append(['0' for i in range(5)])
+        field.insert(0,[i for i in range(6)])               # Number coordinates
+        for char in range(ord('A'),ord('F')):               # Character coordinates
             field[char-ord('A')+1].insert(0, chr(char))
         field[0][0]=' '
         return(field)
@@ -22,18 +19,16 @@ class Battlefield:
             for j in field[i]:
                 print(j,end=' ')
     def check(coordinates):                                 # Are coordinates valid?
-        if len(coordinates) != 2:                           # Looks ugly, need to improve
-            print('Wrong coordinates')
-            return False
-        x, y = coordinates[0], coordinates[1]
+        x, y = coordinates[0], coordinates[1:]
         if x.isalpha() and y.isdigit():
             y = int(y)
-            if 0<y<11 and ('a'<=x<='j' or 'A'<=x<='J'):
-                return ch_coord[x], y
-        if x.isdigit() and y.isalpha():
-            x = int(x)
-            if 0<x<11 and ('a'<=y<='j' or 'A'<=y<='J'):
-                return ch_coord[y], x
+            if 1<=y<=10 and ('a'<=x<='f' or 'A'<=x<='F'):
+                if x.isupper():
+                    return (ord(x)-ord('A')+1), y
+                else: return (ord(x)-ord('a')+1), y
+            else:
+                print('Wrong coordinates')
+                return False
         else:
             print('Wrong coordinates')
             return False
@@ -41,19 +36,27 @@ class Battlefield:
 
 class Player:
     def __init__(self):
-        self.battleships = 1
-        self.battlecruisers = 2
-        self.cruisers = 3
-        self.frigates = 4
-        self.ships = self.battleships + self.battlecruisers + self.cruisers + self.frigates
-        self.field = Battlefield.empty_field
-        self.enemy_field = Battlefield.empty_field
-    #def attack(self):
+        #self.battleships = 1
+        #self.battlecruisers = 2                    #For future
+        #self.cruisers = 3
+        #self.frigates = 4
+        self.ships = 10                             # Number of overall tables with ships
+        self.field = Battlefield.empty_field(Battlefield)
+        self.enemy_field = Battlefield.empty_field(Battlefield)
+    def attack(self):
 
 class Enemy(Player):
     def __init__(self):
         Player.__init__(self)
         self.name = 'Enemy'
+    def fill(self):
+        while self.ships > 0:
+            x, y = randint(1,5), randint(1,5)
+            if self.field[x][y] != '1':
+                self.field[x][y] = '1'
+                self.ships -= 1
+            else: continue
+        Battlefield.show(self.field)
 
 class Human(Player):
     def __init__(self):
@@ -64,16 +67,21 @@ class Human(Player):
     def show(self):
         print("Yours field")
         Battlefield.show(self.field)
-        print("Enemies field")
+        print("\n\nEnemies field")
         Battlefield.show(self.enemy_field)
     def fill(self):
         if self.state != 'Waiting':
             print("Can't do it right now")
-        print('Place your ships\n', 'Enter the coordinates of your ships one by one, like d4,G7 etc.')
+        print('Place your ships','\nEnter the coordinates of your ships one by one, like d4,G7 etc.')
+        print('You have %s ships remain' %self.ships)
         while self.ships > 0:
-            coordinates = input('> ')
-            while Battlefield.check(coordinates) != False:
+            coordinates = input('\n> ')
+            if Battlefield.check(coordinates):
                 x, y = Battlefield.check(coordinates)
-                self.field[x][y]='1'
-                self.ships -= 1
-                Battlefield.show(self.field)
+                if self.field[x][y] == '1':
+                    print('There is already ship in that location')
+                else:
+                    self.field[x][y]='1'
+                    self.ships -= 1
+                    Battlefield.show(self.field)
+                    print('\n\nYou have %s ships remain' %self.ships)
