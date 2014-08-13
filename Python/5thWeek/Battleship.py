7cc784d15aae474f630ee5ad5eac02b9fa51fce2
Alwx <alwxsin@gmail.com>
2014-08-13 16:06:28 +0400

    completed Battlefield

--- Python/5thWeek/Battleship.py
+++ Python/5thWeek/Battleship.py
@@ -1,28 +1,32 @@
 '''Battleship game'''
 from random import randint
-        
+
+all_coord = set()                       # All coordinates, for future checks
+for i in range(1,6):
+    for j in range(ord('a'),ord('f')):
+        x = chr(j)+str(i)
+        all_coord.add(x)
+
 class Battlefield:
-    def __init__self(self):
-        self.name = 'Battlefield'
     def empty_field(self):
-        field=[]                                            # Empty array will fills for battlefield
-        for x in range(0,5):                                # First just zeros
+        field=[]                                                    # Empty array will fills for battlefield
+        for x in range(0,5):                                        # At first just zeros
             field.append(['0' for i in range(5)])
-        field.insert(0,[i for i in range(6)])               # Number coordinates
-        for char in range(ord('A'),ord('F')):               # Character coordinates
-            field[char-ord('A')+1].insert(0, chr(char))
+        field.insert(0,[i for i in range(6)])                       # Number coordinates
+        for char in range(ord('a'),ord('a')):                       # Character coordinates
+            field[char-ord('a')+1].insert(0, chr(char))
         field[0][0]=' '
         return(field)
     def show(field):
-        for i in range(len(field)):                         # Prints nice looking battlefield
+        for i in range(len(field)):                                 # Prints nice looking battlefield
             print('')
             for j in field[i]:
                 print(j,end=' ')
-    def check(coordinates):                                 # Are coordinates valid?
+    def check(coordinates):                                         # Are coordinates valid?
         x, y = coordinates[0], coordinates[1:]
         if x.isalpha() and y.isdigit():
             y = int(y)
-            if 1<=y<=10 and ('a'<=x<='f' or 'A'<=x<='F'):
+            if 1<=y<=5 and ('a'<=x<='f' or 'A'<=x<='F'):
                 if x.isupper():
                     return (ord(x)-ord('A')+1), y
                 else: return (ord(x)-ord('a')+1), y
@@ -36,45 +40,58 @@ class Battlefield:
 
 class Player:
     def __init__(self):
-        #self.battleships = 1
-        #self.battlecruisers = 2                    #For future
-        #self.cruisers = 3
-        #self.frigates = 4
-        self.ships = 10                             # Number of overall tables with ships
+        self.name = ''
+        self.ships = 2                                              # Number of overall tables with ships
         self.field = Battlefield.empty_field(Battlefield)
         self.enemy_field = Battlefield.empty_field(Battlefield)
-    def attack(self):
-
-class Enemy(Player):
-    def __init__(self):
-        Player.__init__(self)
-        self.name = 'Enemy'
-    def fill(self):
-        while self.ships > 0:
+        self.my_coord = all_coord
+        self.killed = 0
+    def fill(self):                                                 # Fill the battlefield in random way
+        count = self.ships
+        while count > 0:
             x, y = randint(1,5), randint(1,5)
             if self.field[x][y] != '1':
                 self.field[x][y] = '1'
-                self.ships -= 1
+                count -= 1
             else: continue
         Battlefield.show(self.field)
-
-class Human(Player):
-    def __init__(self):
-        Player.__init__(self)
-        self.name = 'You'
-        self.state = 'Waiting'
-    #def help:
+    def attack(self):                                               # Function returns False if player missed and True in all other situations
+        coord = self.my_coord.pop()                                 # Takes random coordinate from set of all coordinates
+        x, y = Battlefield.check(coord)
+        if self.enemy_field[x][y] == '0' and p.field[x][y] == '0':  # Miss
+            print('%s misses ' %self.name + coord + '\n------------\n')
+            self.enemy_field[x][y] = 'm'
+            p.field[x][y] = 'm'
+            return False
+        if self.enemy_field[x][y] == 'm':                           # Already hit here
+            return True
+        if p.field[x][y] == '1':                                    # Hit confirmed
+            print('%s HITS ' %self.name + coord + '\n------------\n')
+            self.enemy_field[x][y] = '1'
+            p.field[x][y] = 'X'
+            self.killed += 1
+            return True
+        else: return True
     def show(self):
         print("Yours field")
         Battlefield.show(self.field)
         print("\n\nEnemies field")
         Battlefield.show(self.enemy_field)
-    def fill(self):
-        if self.state != 'Waiting':
-            print("Can't do it right now")
+
+class Enemy(Player):
+    def __init__(self):
+        Player.__init__(self)
+        self.name = 'Enemy'
+
+class Human(Player):
+    def __init__(self):
+        Player.__init__(self)
+        self.name = 'You'
+    def fill_byhands(self):                                         # Fill battlefield manualy
         print('Place your ships','\nEnter the coordinates of your ships one by one, like d4,G7 etc.')
         print('You have %s ships remain' %self.ships)
-        while self.ships > 0:
+        count = self.ships
+        while count > 0:
             coordinates = input('\n> ')
             if Battlefield.check(coordinates):
                 x, y = Battlefield.check(coordinates)
@@ -82,6 +99,63 @@ class Human(Player):
                     print('There is already ship in that location')
                 else:
                     self.field[x][y]='1'
-                    self.ships -= 1
+                    count -= 1
                     Battlefield.show(self.field)
                     print('\n\nYou have %s ships remain' %self.ships)
+    def attack(self, x, y):
+        if self.enemy_field[x][y] == '0' and e.field[x][y] == '0':  # Miss
+            print('\n------------\n'+'You miss'+'\n------------')
+            self.enemy_field[x][y] = 'm'
+            return False
+        if self.enemy_field[x][y] == 'm':                           # Already hit here
+            print('You already hit that place')
+            return True
+        if self.enemy_field[x][y] == '0' and e.field[x][y] == '1':  # Hit cinfirmed
+            print('\n------------\n'+'You HIT'+'\n------------')
+            self.enemy_field[x][y] = 'X'
+            self.killed += 1
+            return True
+        else:
+            print('Check again')
+            return True
+
+p = Human()
+e = Enemy()
+e.fill()
+print("Let's start\n")
+print('If you want to random fill your battlefield, type "random". If you want to fill manually, type "myself"')
+while True:
+    line = input('')
+    if line == 'random': # If player don't want to fill manually
+        p.fill()
+        break
+    if line == 'myself': # If player wants to fill manually
+        p.fill_byhands()
+        break
+    else:
+        print('Check your input')
+
+while True:
+    pendulum = 1                                                    # Just counter
+    if pendulum == 1:
+        coord = input('\nYour move. Print coordinates to fire\n')
+        x, y = Battlefield.check(coord)
+        result = p.attack(x,y)
+        p.show()
+        print('\n------------')
+        if p.killed == p.ships:
+            print('Congratulations. You won')
+            break
+        if not result: pendulum = 0                                 # Move goes to enemy
+    if pendulum == 0:
+        print("\nEnemy's move")
+        result = e.attack()
+        print('\n------------')
+        if e.killed == e.ships:
+            print('You lost')
+            break
+        if not result: pendulum = 1                                 # Move goes to enemy
+        p.show()
+
+# Enemy strike only once if he hits
+# Бот, если попал, должен стрелять еще раз. До тех пор, пока не промахнется. Этого не происходит. Разобраться.
